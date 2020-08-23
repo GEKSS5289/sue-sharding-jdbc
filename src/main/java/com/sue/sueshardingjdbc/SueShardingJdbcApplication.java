52b8187a2b0e23f7f206fa8ce124d09fdd6c5e4e
GEKSS5289 <496966859@qq.com>
2020-08-23 14:23:58 +0800

    构建sue-sharding-jdbc

---
+++ src/main/java/com/sue/sueshardingjdbc/SueShardingJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.sue.sueshardingjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SueShardingJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SueShardingJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sue/sueshardingjdbc/SueShardingJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sue.sueshardingjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SueShardingJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
