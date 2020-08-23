062bb7473af2c15018fc35123c5475de09fbf731
GEKSS5289 <496966859@qq.com>
2020-08-23 15:41:57 +0800

    SpringBoot Properties方式配置sharding-jdbc

--- src/main/java/com/sue/sueshardingjdbc/SueShardingJdbcApplication.java
+++ src/main/java/com/sue/sueshardingjdbc/SueShardingJdbcApplication.java
@@ -2,12 +2,14 @@ package com.sue.sueshardingjdbc;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:sharding-jdbc.xml")
 @MapperScan("com.sue.mapper")
+
 public class SueShardingJdbcApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/sue/sueshardingjdbc/SueShardingJdbcApplicationTests.java
+++ src/test/java/com/sue/sueshardingjdbc/SueShardingJdbcApplicationTests.java
@@ -1,13 +1,16 @@
 package com.sue.sueshardingjdbc;
 
 
+import com.sue.mapper.AreaMapper;
 import com.sue.mapper.OrderMapper;
+import com.sue.pojo.Area;
 import com.sue.pojo.Order;
 import org.aspectj.weaver.ast.Or;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
@@ -19,6 +22,8 @@ public class SueShardingJdbcApplicationTests {
     @Autowired
     private OrderMapper orderMapper;
 
+    @Autowired
+    private AreaMapper areaMapp;
 
     @Test
     public void contextLoads() {
@@ -27,10 +32,18 @@ public class SueShardingJdbcApplicationTests {
     @Test
     public void testOrder(){
         Order order = new Order();
-        order.setUserId(19);
+        order.setUserId(20);
         order.setId(1);
         order.setOrderAmount(BigDecimal.TEN);
-        order.setOrderStatus(1);
+        order.setOrderStatus(4);
         orderMapper.insert(order);
+    }
+
+    @Test
+    public void testGlobal(){
+        Area area = new Area();
+        area.setId(1);
+        area.setName("北京");
+        areaMapp.insert(area);
     }
 }
